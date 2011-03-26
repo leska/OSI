160e01b7f146540b59f30e5f87531726d15ce4c0
leska <lesya.zakirova@gmail.com>
2011-03-26 14:48:50 +0500

    errors fixed

--- Sort/sort.c
+++ Sort/sort.c
@@ -11,27 +11,29 @@ int arr[MAXNUMBERS], count;
 int main(int argc, char** argv) {
 	int i,j,t, readed, writed;
 
-	if (argc < 2)
+	if (argc < 3)
 		die("malo faylow");
 	count = 0;
 
-	for(i = 1; i < argc-1; i++){	
+	for(i = 1; i < argc-1; i++){
 		FILE * f = fopen(argv[i], "r");
 		if (!f)
 			die("Bad");
 		while (!feof(f)){
 			readed = fscanf(f, "%d", &arr[count++]);
-			if (readed <= 0){
+			if (readed == 0){
 				fclose(f);
 				die("v fayle kaka");
 			}
+			else if (readed == EOF)
+				count--;
 			if (count >= MAXNUMBERS){
 				fclose(f);
 				die("U menya ne hvataet pamati");
 			}
 		}
 		fclose(f);
-	}	
+	}
 
 	for(i = 0; i < count; i++)
 		for(j = 0; j < i; j++){
